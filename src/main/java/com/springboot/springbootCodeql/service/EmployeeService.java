f9c6c82f2f8794e20b6bd1b4b77c5473147db5a0
fayaz1420 <fayaz14@nmsu.edu>
2026-04-23 14:29:23 -0500

    search-api

--- src/main/java/com/springboot/springbootCodeql/service/EmployeeService.java
+++ src/main/java/com/springboot/springbootCodeql/service/EmployeeService.java
@@ -46,6 +46,7 @@ public class EmployeeService {
         return repository.save(existing);
     }
 
+    // search for employees in the system based on their name or department
     public void delete(Long id) {
         repository.deleteById(id);
     }
